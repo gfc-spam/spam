051a3e9fdff0421a14589dc0ef5dfd41b88969e1
gfc-spam <58083152+gfc-spam@users.noreply.github.com>
2019-11-22 23:12:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,64 +5,44 @@ console.log("⪧ SPAMMER ⪦");
 client.login(process.env.TOKEN);
 
 client.on("ready", () => {
-let channel =     client.channels.get(process.env.CHANNEL)
+let channel =     client.channels.get("646730428530556938")
 setInterval(function() {
-channel.send(process.env.MESSAGE);
+channel.send("| Loading Credits for gaming factor... | ...جارِ تحميل الكريديتس لجيمينج فاكتور |");
 }, 30)
 })
 
 client.on("message", async message => {
     const prefix = "-";
 
-    // If the author's a bot, return
-    // If the message was not sent in a server, return
-    // If the message doesn't start with the prefix, return
+    if (message.author.bot) return;
     if (!message.guild) return;
     if (!message.content.startsWith(prefix)) return;
     if(!message.member.roles.has("647529309464166451")) return;
 
-    // Arguments and command variable
-    // cmd is the first word in the message, aka the command
-    // args is an array of words after the command
-    // !say hello I am a bot
-    // cmd == say (because the prefix is sliced off)
-    // args == ["hello", "I", "am", "a", "bot"]
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
-
     if (cmd === "ping") {
-        // Send a message
         const msg = await message.channel.send(`🏓 Pinging....`);
-
-        // Edit the message
-        msg.edit(`🏓 Pong!\nLatency is ${Math.floor(msg.createdTimestap - message.createdTimestap)}ms\nAPI Latency is ${Math.round(client.ping)}ms`);
+        msg.edit(`🏓 Pong!\nYour ping is : ${Math.round(client.ping)}ms`);
     }
 
     if (cmd === "say") {
-        // Check if you can delete the message
-        if (message.deletable) message.delete();
-
-        if (args.length < 0) return message.reply(`Nothing to say?`).then(m => m.delete(5000));
-        
-        // Role color
-        const roleColor = message.guild.me.highestRole.hexColor;
-
-        // If the first argument is embed, send an embed,
-        // otherwise, send a normal message
-        if (args[0].toLowerCase() === "embed") {
-            const embed = new RichEmbed()
-                .setDescription(args.slice(1).join(" "))
-                .setColor(roleColor === "#000000" ? "#ffffff" :  roleColorv)
-                .setTimestamp()
-                .setImage(client.user.displayAvatarURL)
-                .setAuthor(message.author.username, message.author.displayAvatarURL);
-
-            message.channel.send(embed);
-        } else if (args[0].toLowerCase() === user.id) {
-        message.channel.send(args.slice(1).join(" "))}
-        else {
             message.channel.send(args.join(" "));
+    }
+    if (cmd === "spsay") {
+        if (args[0].toLowerCase() === process.env.INDEX ) {
+            message.channel.send(args.slice(1).join(" "));
         }
+    }
+    if (cmd === "scsay") {
+        if (message.deletable) message.delete();
+            message.channel.send(args.join(" "));
         
+    }
+    if (cmd === "spscsay") {
+        if (message.deletable) message.delete();
+        if (args[0].toLowerCase() === process.env.INDEX ) {
+            message.channel.send(args.slice(1).join(" "));
+        }
     }
 });
