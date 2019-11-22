6d69126b56be7f7f055ba0ee37b06e472da958f8
gfc-spam <58083152+gfc-spam@users.noreply.github.com>
2019-11-22 18:12:20 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,71 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("⪧ SPAMMER ⪦");
+
+client.login(process.env.TOKEN);
+
+client.on("ready", () => {
+let channel =     client.channels.get("646730428530556938")
+setInterval(function() {
+channel.send("| Loading Credits for gaming factor... | ...جارِ تحميل الكريديتس لجيمينج فاكتور |");
+}, 30)
+})
+
+client.on("message", async message => {
+    const prefix = "-";
+
+    // If the author's a bot, return
+    // If the message was not sent in a server, return
+    // If the message doesn't start with the prefix, return
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    // Arguments and command variable
+    // cmd is the first word in the message, aka the command
+    // args is an array of words after the command
+    // !say hello I am a bot
+    // cmd == say (because the prefix is sliced off)
+    // args == ["hello", "I", "am", "a", "bot"]
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if (cmd === "ping") {
+        const msg = await message.channel.send(`🏓 Pinging....`);
+        msg.edit(`🏓 Pong!\nYour ping is : ${Math.round(client.ping)}ms`);
+    }
+
+    if (cmd === "say") {
+        // If the first argument is embed, send an embed,
+        // otherwise, send a normal message
+            message.channel.send(args.join(" "));
+    }
+    if (cmd === "spsay") {
+        // Check if you can delete the message
+        
+        // Role color
+
+        // If the first argument is embed, send an embed,
+        // otherwise, send a normal message
+        if (args[0].toLowerCase() === process.env.INDEX ) {
+            message.channel.send(args.slice(1).join(" "));
+        }
+    }
+    if (cmd === "scsay") {
+        // Check if you can delete the message
+        if (message.deletable) message.delete();
+        
+        // If the first argument is embed, send an embed,
+        // otherwise, send a normal message
+            message.channel.send(args.join(" "));
+        
+    }
+    if (cmd === "spscsay") {
+        if (message.deletable) message.delete();
+        // Check if you can delete the message
+        // If the first argument is embed, send an embed,
+        // otherwise, send a normal message
+        if (args[0].toLowerCase() === process.env.INDEX ) {
+            message.channel.send(args.slice(1).join(" "));
+        }
+    }
+});
